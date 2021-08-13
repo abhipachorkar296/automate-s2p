85a6b4a272438c3e159b9ac55c68b62b18b534c0
abhipachorkar296 <45617019+abhipachorkar296@users.noreply.github.com>
2021-08-13 17:58:33 +0530

    chane in key

--- factwise/settings.py
+++ factwise/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9&#nn=cq&ojhf8ptgba#ecjsam*b2(ld*kh1a_x1taqv8est+8'
+# SECRET_KEY = '9&#nn=cq&ojhf8ptgba#ecjsam*b2(ld*kh1a_x1taqv8est+8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
